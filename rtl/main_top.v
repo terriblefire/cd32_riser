bea931ffc466070a3003fc71e8638e8dfbd46651
sleary78 <github@vavi.co.uk>
2020-01-19 12:36:18 +0000

    FIxing stupid rtl issues

--- rtl/main_top.v
+++ rtl/main_top.v
@@ -21,9 +21,11 @@ module main_top(
     input AS20, 
     input DS20, 
     input RW, 
+    input [1:0] SIZ,
     input [23:0] A,
     
     inout [31:24] D,
+    output [1:0] DSACK,
 
     // Punting... 
     input PUNT_IN, 
@@ -39,6 +41,10 @@ module main_top(
     input TXD, 
     output RXD, 
 
+    output KB_CLOCK, 
+    output KB_DATA,
+
+
     // SPI COMMS 
 
     input SPI_CK, 
@@ -48,26 +54,46 @@ module main_top(
 
 );
 
+wire chipset_decode = A[23:12] != 12'hDFF;
+wire ciaa_decode = A[23:12] != 12'hBFE;
+wire ciab_decode = A[23:12] != 12'hBFD;
+
+wire [5:0] joya;
+wire [5:0] joyb;
+
+
 
 //// user io has an extra spi channel outside minimig core ////
 user_io user_io(
-     .SPI_CLK(SPI_SCK),
+
+     .SPI_CLK(SPI_CK),
      .SPI_SS_IO(SPI_NSS),
      .SPI_MISO(SPI_MISO),
      .SPI_MOSI(SPI_MOSI),
+
      .JOY0(joya),
      .JOY1(joyb),
+     
      .MOUSE_BUTTONS(mouse_buttons),
+     
      .KBD_MOUSE_DATA(kbd_mouse_data),
      .KBD_MOUSE_TYPE(kbd_mouse_type),
      .KBD_MOUSE_STROBE(kbd_mouse_strobe),
+     
      .CORE_TYPE(8'ha1),    // minimig core id
-	 .BUTTONS(board_buttons),
+	 
+     .BUTTONS(board_buttons),
 	 .SWITCHES(board_switches)
   );
+
+
+
 
 // bare min to make the riser work with a card.
 assign PUNT_OUT = PUNT_IN ? 1'bz : 1'b0;
 
+// pass the amiga serial port to the ARM so we can see its debug output
+assign UART5_RX = TXD;
+assign RXD = UART5_TX;
 
 endmodule
